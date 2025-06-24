c9990766a206c986dda08fb14935e07874a84a3a
set=Charan M <charanm@clarium.tech>
2025-06-24 12:10:18 +0530

    structural-directive-ng-for-vs-for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StructuralDirectivesNgIfVsIfComponent } from "./structural-directives-ng-if-vs-if/structural-directives-ng-if-vs-if.component";
+import { StructuralDirectivesNgForVsForComponent } from "./structural-directives-ng-for-vs-for/structural-directives-ng-for-vs-for.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StructuralDirectivesNgIfVsIfComponent],
+  imports: [RouterOutlet, StructuralDirectivesNgIfVsIfComponent, StructuralDirectivesNgForVsForComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
